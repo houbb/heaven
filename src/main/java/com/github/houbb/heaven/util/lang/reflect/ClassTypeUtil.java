1ab33e890a54e44e6046a5f174293528c70b4e8f
binbin.hou <1060732496@qq.com>
2019-03-03 09:44:56 +0800

    [Fixed] 修正对于 java bean 的判断

--- src/main/java/com/github/houbb/heaven/util/lang/reflect/ClassTypeUtil.java
+++ src/main/java/com/github/houbb/heaven/util/lang/reflect/ClassTypeUtil.java
@@ -123,7 +123,8 @@ public class ClassTypeUtil {
                 && !clazz.isAnnotation()
                 && !clazz.isSynthetic()
                 && !clazz.isPrimitive()
-                && isIterable(clazz);
+                && !isIterable(clazz)
+                && !isMap(clazz);
     }
 
     /**
