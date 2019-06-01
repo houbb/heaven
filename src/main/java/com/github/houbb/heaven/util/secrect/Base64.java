df2e7776280c7e1e2a0079df9a06161aa4b53778
binbin.hou <1060732496@qq.com>
2019-06-01 16:07:55 +0800

    [Feature] add for new

--- src/main/java/com/github/houbb/heaven/util/secrect/Base64.java
+++ src/main/java/com/github/houbb/heaven/util/secrect/Base64.java
@@ -19,6 +19,7 @@ public final class Base64 {
      * 将字节数组转换成十六进制，并以字符串的形式返回
      * 128位是指二进制位。二进制太长，所以一般都改写成16进制，
      * 每一位16进制数可以代替4位二进制数，所以128位二进制数写成16进制就变成了128/4=32位。
+     * @param bytes 字节流
      */
     public static String toBase64String(byte[] bytes){
         StringBuilder sb = new StringBuilder();
@@ -30,6 +31,7 @@ public final class Base64 {
 
     /**
      * 将一个字节转换成十六进制，并以字符串的形式返回
+     * @param b 比特
      */
     private static String byteToHex(byte b) {
         int n = b;
