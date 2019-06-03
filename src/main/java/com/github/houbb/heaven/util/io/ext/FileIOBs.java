6ea59d3ecc4f25d7b40dd1ef0ec05d52116082cc
binbin.hou <1060732496@qq.com>
2019-06-03 10:43:25 +0800

    [Feature] add for file random

---
+++ src/main/java/com/github/houbb/heaven/util/io/ext/FileIOBs.java
@@ -0,0 +1,9 @@
+package com.github.houbb.heaven.util.io.ext;
+
+/**
+ * 文件 IO 引导类
+ * @author binbin.hou
+ * @since 0.1.9
+ */
+public class FileIOBs {
+}
---
+++ src/main/java/com/github/houbb/heaven/util/io/ext/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * IO 拓展内容
+ * 比如 {@link java.io.RandomAccessFile}
+ * (2) {@link java.nio.MappedByteBuffer} 这个放在 nio 中。
+ * @author binbin.hou
+ * @since 0.1.9
+ */
+package com.github.houbb.heaven.util.io.ext;
