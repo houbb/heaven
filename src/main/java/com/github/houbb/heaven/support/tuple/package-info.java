f6272d064141b1652eddfeb3b80545a7f40a34e0
binbin.hou <1060732496@qq.com>
2019-06-04 20:38:41 +0800

    release branch 0.1.9

--- src/main/java/com/github/houbb/heaven/support/tuple/package-info.java
+++ src/main/java/com/github/houbb/heaven/support/tuple/package-info.java
@@ -1,16 +1,5 @@
 /**
  * 元组
- * Unit<A> (1 element)
- * Pair<A,B> (2 elements)
- * Triplet<A,B,C> (3 elements)
- * Quartet<A,B,C,D> (4 elements)
- * Quintet<A,B,C,D,E> (5 elements)
- * Sextet<A,B,C,D,E,F> (6 elements)
- * Septet<A,B,C,D,E,F,G> (7 elements)
- * Octet<A,B,C,D,E,F,G,H> (8 elements)
- * Ennead<A,B,C,D,E,F,G,H,I> (9 elements)
- * Decade<A,B,C,D,E,F,G,H,I,J> (10 elements)
- *
  * 为了避免平时代码两个属性就要建立一个对象的窘境。
  *
  * （1）此处只实现 2/3/4 这三种情况。
