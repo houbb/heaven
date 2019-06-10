af28210f5d5d4374ddd91ebf3a00932592dbb25c
houbb <1060732496@qq.com>
2019-06-10 22:47:10 +0800

    release branch 0.1.11

--- src/main/java/com/github/houbb/heaven/constant/SystemConst.java
+++ src/main/java/com/github/houbb/heaven/constant/SystemConst.java
@@ -14,145 +14,145 @@ public final class SystemConst {
     /**
      * Java 运行时环境版本
      */
-    private static final String VERSION = "java.version";
+    public static final String VERSION = "java.version";
 
     /**
      * Java 运行时环境供应商
      */
-    private static final String VENDOR = "java.vendor";
+    public static final String VENDOR = "java.vendor";
 
     /**
      * Java 供应商的 URL
      */
-    private static final String VENDOR_URL = "java.vendor.url";
+    public static final String VENDOR_URL = "java.vendor.url";
 
     /**
      * Java 安装目录
      */
-    private static final String HOME = "java.home";
+    public static final String HOME = "java.home";
 
     /**
      * Java 虚拟机规范版本
      */
-    private static final String VM_SPECIFICATION_VERSION = "java.vm.specification.version";
+    public static final String VM_SPECIFICATION_VERSION = "java.vm.specification.version";
 
     /**
      * Java 虚拟机规范供应商
      */
-    private static final String VM_SPECIFICATION_VENDOR = "java.vm.specification.vendor";
+    public static final String VM_SPECIFICATION_VENDOR = "java.vm.specification.vendor";
 
     /**
      * Java 虚拟机规范名称
      */
-    private static final String VM_SPECIFICATION_NAME = "java.vm.specification.name";
+    public static final String VM_SPECIFICATION_NAME = "java.vm.specification.name";
 
     /**
      * Java 虚拟机实现版本
      */
-    private static final String VM_VERSION = "java.vm.version";
+    public static final String VM_VERSION = "java.vm.version";
 
     /**
      * Java 虚拟机实现供应商
      */
-    private static final String VM_VENDOR = "java.vm.vendor";
+    public static final String VM_VENDOR = "java.vm.vendor";
 
     /**
      * Java 虚拟机实现名称
      */
-    private static final String VM_NAME = "java.vm.name";
+    public static final String VM_NAME = "java.vm.name";
 
     /**
      * Java 运行时环境规范版本
      */
-    private static final String SPECIFICATION_VERSION = "java.specification.version";
+    public static final String SPECIFICATION_VERSION = "java.specification.version";
 
     /**
      * Java 运行时环境规范供应商
      */
-    private static final String SPECIFICATION_VENDOR = "java.specification.vendor";
+    public static final String SPECIFICATION_VENDOR = "java.specification.vendor";
 
     /**
      * Java 运行时环境规范名称
      */
-    private static final String SPECIFICATION_NAME = "java.specification.name";
+    public static final String SPECIFICATION_NAME = "java.specification.name";
 
     /**
      * Java 类格式版本号
      */
-    private static final String CLASS_VERSION = "java.class.version";
+    public static final String CLASS_VERSION = "java.class.version";
 
     /**
      * 类路径
      */
-    private static final String CLASS_PATH = "java.class.path";
+    public static final String CLASS_PATH = "java.class.path";
 
     /**
      *  加载库时搜索的路径列表
      */
-    private static final String LIBRARY_PATH = "java.library.path";
+    public static final String LIBRARY_PATH = "java.library.path";
 
     /**
      *  默认的临时文件路径
      */
-    private static final String IO_TMPDIR = "java.io.tmpdir";
+    public static final String IO_TMPDIR = "java.io.tmpdir";
 
     /**
      * 要使用的 JIT 编译器的名称
      */
-    private static final String COMPILER = "java.compiler";
+    public static final String COMPILER = "java.compiler";
 
     /**
      * 一个或多个扩展目录的路径
      */
-    private static final String EXT_DIRS = "java.ext.dirs";
+    public static final String EXT_DIRS = "java.ext.dirs";
 
     /**
      * 操作系统的名称
      */
-    private static final String OS_NAME = "os.name";
+    public static final String OS_NAME = "os.name";
 
     /**
      * 操作系统的架构
      */
-    private static final String OS_ARCH = "os.arch";
+    public static final String OS_ARCH = "os.arch";
 
     /**
      * 操作系统的版本
      */
-    private static final String OS_VERSION = "os.version";
+    public static final String OS_VERSION = "os.version";
 
     /**
      * 文件分隔符
      * UNIX /
      */
-    private static final String FILE_SEPARATOR = "file.separator";
+    public static final String FILE_SEPARATOR = "file.separator";
 
     /**
      * 路径分隔符
      * Unix :
      */
-    private static final String PATH_SEPARATOR = "path.separator";
+    public static final String PATH_SEPARATOR = "path.separator";
 
     /**
      * 行分隔符
      * Unix /n
      */
-    private static final String LINE_SEPARATOR = "line.separator";
+    public static final String LINE_SEPARATOR = "line.separator";
 
     /**
      * 用户的账户名称
      */
-    private static final String USER_NAME = "user.name";
+    public static final String USER_NAME = "user.name";
 
     /**
      * 用户的主目录
      */
-    private static final String USER_HOME = "user.home";
+    public static final String USER_HOME = "user.home";
 
     /**
      * 户的当前工作目录
      */
-    private static final String USER_DIR = "user.dir";
+    public static final String USER_DIR = "user.dir";
 
     /**
      * 获取指定配置信息
