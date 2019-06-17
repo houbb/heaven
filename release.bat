aa0c787b9c8f69b7a302283c13902085c0d6ec20
binbin.hou <1060732496@qq.com>
2019-06-17 20:01:12 +0800

    [Feature] add for new

--- release.bat
+++ release.bat
@@ -10,9 +10,9 @@ ECHO "============================= RELEASE START..."
 
 :: 版本号信息(需要手动指定)
 :::: 旧版本名称
-SET version=0.1.14
+SET version=0.1.15
 :::: 新版本名称
-SET newVersion=0.1.15
+SET newVersion=0.1.16
 :::: 组织名称
 SET groupName=com.github.houbb
 :::: 项目名称
