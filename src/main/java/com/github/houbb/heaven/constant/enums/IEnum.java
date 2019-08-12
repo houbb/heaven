afbfc8df2e1e6f5704059ec892aae29d86b7bc8e
binbin.hou <1060732496@qq.com>
2019-08-12 19:20:29 +0800

    release branch 0.1.19

---
+++ src/main/java/com/github/houbb/heaven/constant/enums/IEnum.java
@@ -0,0 +1,22 @@
+package com.github.houbb.heaven.constant.enums;
+
+/**
+ * 枚举接口
+ * @author binbin.hou
+ * @since 0.1.19
+ */
+public interface IEnum {
+
+    /**
+     * 获取编码
+     * @return 编码
+     */
+    String getCode();
+
+    /**
+     * 获取消息
+     * @return 消息
+     */
+    String getMsg();
+
+}
