5d34863c17b6179f7cb61c9704cf7c2a7cd0ba2b
houbb <1060732496@qq.com>
2019-10-22 21:33:39 +0800

    [Feature] add for new

--- src/main/java/com/github/houbb/heaven/util/lang/reflect/ReflectMethodUtil.java
+++ src/main/java/com/github/houbb/heaven/util/lang/reflect/ReflectMethodUtil.java
@@ -5,9 +5,17 @@
 
 package com.github.houbb.heaven.util.lang.reflect;
 
+import com.github.houbb.heaven.support.handler.IHandler;
+import com.github.houbb.heaven.util.common.ArgUtil;
+import com.github.houbb.heaven.util.util.ArrayUtil;
+
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * 反射方法工具类
@@ -18,6 +26,67 @@ import java.lang.reflect.Type;
 public final class ReflectMethodUtil {
 
     private ReflectMethodUtil() {}
+
+    /**
+     * 忽略的方法名称列表
+     * （1）object 默认方法
+     * （2）class 默认方法
+     *
+     * 可优化方案：
+     * 将所有方法写死，放在列表中、
+     * 缺点：占地方，无法动态更新。
+     * @since 0.1.36
+     */
+    private static final List<String> IGNORE_METHOD_LIST;
+
+    static {
+        Set<String> methodNameSet = new HashSet<>(64);
+        for(Method method : Object.class.getMethods()) {
+            methodNameSet.add(method.getName());
+        }
+        for(Method method : Class.class.getMethods()) {
+            methodNameSet.add(method.getName());
+        }
+        IGNORE_METHOD_LIST = new ArrayList<>(methodNameSet);
+    }
+
+    /**
+     * 获取忽略的方法列表
+     * @return 忽略方法名称列表
+     * @since 0.1.36
+     */
+    public static List<String> getIgnoreMethodList() {
+        return IGNORE_METHOD_LIST;
+    }
+
+    /**
+     * 是否为应该忽略的方法名称
+     * @param methodName 方法名称
+     * @return 是否
+     * @since 0.1.36
+     */
+    public static boolean isIgnoreMethod(final String methodName) {
+        return getIgnoreMethodList().contains(methodName);
+    }
+
+    /**
+     * 获取方法类型的名称
+     * @param method 方法反射信息
+     * @return 参数列表
+     * @since 0.1.36
+     */
+    public static List<String> getParamTypeNames(final Method method) {
+        ArgUtil.notNull(method, "method");
+
+        Class<?>[] paramTypes = method.getParameterTypes();
+
+        return ArrayUtil.toList(paramTypes, new IHandler<Class<?>, String>() {
+            @Override
+            public String handle(Class<?> aClass) {
+                return aClass.getName();
+            }
+        });
+    }
 
     /**
      * 获取方法返回值的泛型
