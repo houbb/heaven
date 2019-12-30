d65e0e346793433903929f266fe61ceb94cb3b98
binbin.hou <1060732496@qq.com>
2019-12-30 21:07:36 +0800

    [Feature] add for new

--- src/main/java/com/github/houbb/heaven/util/net/EncodeUtil.java
+++ src/main/java/com/github/houbb/heaven/util/net/EncodeUtil.java
@@ -1,12 +1,14 @@
 package com.github.houbb.heaven.util.net;
 
 import com.github.houbb.heaven.constant.CharsetConst;
+import com.github.houbb.heaven.util.lang.StringUtil;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 /**
  * 编码工具类
+ *
  * @author binbin.hou
  * @since 0.1.1
  */
@@ -14,6 +16,7 @@ public class EncodeUtil {
 
     /**
      * 对内容进行 utf=8 编码
+     *
      * @param content 编码
      * @return 结果
      */
@@ -23,6 +26,7 @@ public class EncodeUtil {
 
     /**
      * 对内容进行 utf=8 编码
+     *
      * @param content 编码
      * @param charset 字符编码
      * @return 结果
@@ -33,6 +37,101 @@ public class EncodeUtil {
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * 编码为 unicode
+     *
+     * @param string 字符串
+     * @return 结果
+     * @since 0.1.66
+     */
+    public static String encodeUnicode(final String string) {
+        if (StringUtil.isEmpty(string)) {
+            return string;
+        }
+
+        String tmp;
+        int length = 6 * string.length();
+        StringBuffer sb = new StringBuffer(length);
+        char c;
+        int i, j;
+        sb.setLength(0);
+        for (i = 0; i < string.length(); i++) {
+            c = string.charAt(i);
+            sb.append("\\u");
+            //取出高8位
+            j = (c >>> 8);
+            tmp = Integer.toHexString(j);
+            if (tmp.length() == 1) {
+                sb.append("0");
+            }
+            sb.append(tmp);
+            //取出低8位
+            j = (c & 0xFF);
+            tmp = Integer.toHexString(j);
+            if (tmp.length() == 1) {
+                sb.append("0");
+            }
+            sb.append(tmp);
+        }
+
+        return (new String(sb));
+    }
+
+    /**
+     * 解码 unicode
+     *
+     * @param string 字符串
+     * @return 结果
+     * @since 0.1.66
+     */
+    public static String decodeUnicode(final String string) {
+        if (StringUtil.isEmpty(string)) {
+            return string;
+        }
+        //如果不是unicode码则原样返回
+        if (!string.contains("\\u")) {
+            return string;
+        }
+
+        int actualLength = string.length() / 6;
+        StringBuilder sb = new StringBuilder(actualLength);
+        for (int i = 0; i <= string.length() - 6; i += 6) {
+            String strTemp = string.substring(i, i + 6);
+            String value = strTemp.substring(2);
+            int c = 0;
+            for (int j = 0; j < value.length(); j++) {
+                char tempChar = value.charAt(j);
+                int t = 0;
+                switch (tempChar) {
+                    case 'a':
+                        t = 10;
+                        break;
+                    case 'b':
+                        t = 11;
+                        break;
+                    case 'c':
+                        t = 12;
+                        break;
+                    case 'd':
+                        t = 13;
+                        break;
+                    case 'e':
+                        t = 14;
+                        break;
+                    case 'f':
+                        t = 15;
+                        break;
+                    default:
+                        t = tempChar - 48;
+                        break;
+                }
+                c += t * ((int) Math.pow(16, (value.length() - j - 1)));
+            }
+            sb.append((char) c);
+        }
+        return sb.toString();
     }
 
 }
