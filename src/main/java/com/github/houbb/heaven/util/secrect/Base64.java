65c679a37d975cc64df6e97513757938a8bd86c0
binbin.hou <binbin.hou@huifu.com>
2020-06-17 10:48:48 +0800

    release branch 0.1.106

--- src/main/java/com/github/houbb/heaven/util/secrect/Base64.java
+++ src/main/java/com/github/houbb/heaven/util/secrect/Base64.java
@@ -1,5 +1,8 @@
 package com.github.houbb.heaven.util.secrect;
 
+import com.github.houbb.heaven.response.exception.CommonRuntimeException;
+import com.github.houbb.heaven.util.lang.StringUtil;
+
 /**
  * base64
  * @author binbin.hou
@@ -14,6 +17,34 @@ public final class Base64 {
      */
     private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 
+    /**
+     * Base64 标准字符集合
+     * @since 0.1.106
+     */
+    private static final char[] ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=".toCharArray();
+
+    /**
+     * 编码信息
+     * @since 0.1.106
+     */
+    private static final byte[] CODES = new byte[256];
+
+    static {
+        for (int i = 0; i < 256; i++) {
+            CODES[i] = -1;
+        }
+        for (int i = 'A'; i <= 'Z'; i++) {
+            CODES[i] = (byte) (i - 'A');
+        }
+        for (int i = 'a'; i <= 'z'; i++) {
+            CODES[i] = (byte) (26 + i - 'a');
+        }
+        for (int i = '0'; i <= '9'; i++) {
+            CODES[i] = (byte) (52 + i - '0');
+        }
+        CODES['+'] = 62;
+        CODES['/'] = 63;
+    }
 
     /**
      * 将字节数组转换成十六进制，并以字符串的形式返回
@@ -47,5 +78,98 @@ public final class Base64 {
         return charOne+charTwo;
     }
 
+    /**
+     * 将原始数据编码为base64编码
+     * @since 0.1.106
+     */
+    public static char[] encode(byte[] data) {
+        char[] out = new char[((data.length + 2) / 3) * 4];
+
+        for (int i = 0, index = 0; i < data.length; i += 3, index += 4) {
+            boolean quad = false;
+            boolean trip = false;
+            int val = (0xFF & (int) data[i]);
+            val <<= 8;
+            if ((i + 1) < data.length) {
+                val |= (0xFF & (int) data[i + 1]);
+                trip = true;
+            }
+            val <<= 8;
+            if ((i + 2) < data.length) {
+                val |= (0xFF & (int) data[i + 2]);
+                quad = true;
+            }
+            out[index + 3] = ALPHABET[(quad ? (val & 0x3F) : 64)];
+            val >>= 6;
+            out[index + 2] = ALPHABET[(trip ? (val & 0x3F) : 64)];
+            val >>= 6;
+            out[index + 1] = ALPHABET[val & 0x3F];
+            val >>= 6;
+            out[index] = ALPHABET[val & 0x3F];
+        }
+
+        return out;
+    }
+
+    /**
+     * 将base64编码的数据解码成原始数据
+     * @since 0.1.106
+     */
+    public static byte[] decode(char[] data)
+    {
+        int len = ((data.length + 3) / 4) * 3;
+        if(data.length > 0 && data[data.length - 1] == '=') {
+            --len;
+        }
+        if(data.length > 1 && data[data.length - 2] == '=') {
+            --len;
+        }
+        byte[] out = new byte[len];
+        int shift = 0;
+        int accum = 0;
+        int index = 0;
+        for (char aData : data) {
+            int value = CODES[aData & 0xFF];
+            if (value >= 0) {
+                accum <<= 6;
+                shift += 6;
+                accum |= value;
+                if (shift >= 8) {
+                    shift -= 8;
+                    out[index++] = (byte) ((accum >> shift) & 0xff);
+                }
+            }
+        }
+        if(index != out.length) {
+            throw new CommonRuntimeException("miscalculated data length!");
+        }
+        return out;
+    }
+
+    /**
+     * 编码
+     * @param text 文本
+     * @return 结果
+     * @since 0.1.106
+     */
+    public static String encode(final String text) {
+        if(StringUtil.isEmpty(text)) {
+            return text;
+        }
+        return new String(encode(text.getBytes()));
+    }
+
+    /**
+     * 解码
+     * @param text 文本
+     * @return 结果
+     * @since 0.1.106
+     */
+    public static String decode(final String text) {
+        if(StringUtil.isEmpty(text)) {
+            return text;
+        }
+        return new String(decode(text.toCharArray()));
+    }
 
 }
