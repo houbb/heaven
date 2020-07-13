ffedfd98b0cc90a5860776c0c50e94c4e2cbda1e
binbin.hou <binbin.hou@huifu.com>
2020-07-13 09:05:28 +0800

    release branch 0.1.112

--- release.bat
+++ release.bat
@@ -10,9 +10,9 @@ ECHO "============================= RELEASE START..."
 
 :: 版本号信息(需要手动指定)
 :::: 旧版本名称
-SET version=0.1.111
+SET version=0.1.112
 :::: 新版本名称
-SET newVersion=0.1.112
+SET newVersion=0.1.113
 :::: 组织名称
 SET groupName=com.github.houbb
 :::: 项目名称
