7d22085d0b4dc09a4ad9c6fbebc44d05261ffecf
binbin.hou <binbin.hou@huifu.com>
2022-12-20 10:28:42 +0800

    release branch 0.1.169

--- src/main/java/com/github/houbb/heaven/util/lang/reflect/ClassUtil.java
+++ src/main/java/com/github/houbb/heaven/util/lang/reflect/ClassUtil.java
@@ -396,4 +396,39 @@ public final class ClassUtil {
 
     }
 
+    /**
+     * 是否是指定接口的子类
+     *
+     * 目的：不引入接口的情况下，做关系的判断
+     *
+     * TODO: 拓展，添加一下父类的判断
+     * @param clazz 类
+     * @param interfaceName 接口名称
+     * @return 结果
+     * @since 0.0.169
+     */
+    public static boolean instanceOf(final Class<?> clazz,
+                                     final String interfaceName) {
+        ArgUtil.notNull(clazz, "clazz");
+        ArgUtil.notEmpty(interfaceName, "interfaceName");
+
+        // 就是自己
+        if(clazz.getName().equals(interfaceName)) {
+            return true;
+        }
+
+        List<Class> interfaces = ClassUtil.getAllInterfaces(clazz);
+        if(CollectionUtil.isEmpty(interfaces)) {
+            return false;
+        }
+
+        for(Class<?> clazzInter : interfaces) {
+            String name = clazzInter.getName();
+            if(name.equals(interfaceName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
